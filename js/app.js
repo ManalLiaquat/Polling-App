b3aef4dbaa27d309d400a5715bc941ff6509dd37
ManalLiaquat <m.manalliaquat@gmail.com>
2018-06-28 01:33:12 +0500

    This is the script file of thi app

--- js/app.js
+++ js/app.js
@@ -34,6 +34,8 @@ function logOut() {
 /* setting db */
 
 function uploadPoll() {
+    var currentUser = firebase.auth().currentUser.uid;
+    console.log(currentUser, 'currentUser')
 
     var pollHeading = document.getElementById("pollHeading");
     var question = document.getElementById("question");
@@ -44,10 +46,11 @@ function uploadPoll() {
         heading: pollHeading.value,
         question: question.value,
         optionA: optionA.value,
-        optionB: optionB.value
+        optionB: optionB.value,
+        uid: currentUser
     }
 
-    firebaseDB.child("App Data").push(myFormData)
+    firebaseDB.child("AppData/Polling").push(myFormData)
         .then(() => {
             pollHeading.value = "";
             question.value = "";
@@ -56,55 +59,76 @@ function uploadPoll() {
         });
 }
 
+var ul = document.getElementById("pollings");
 function getData() {
-    firebaseDB.child("App Data").once('value', (data) => {
-
-        var postObject = data.val();
-        console.log(postObject, 'postObject');
-        var keys = Object.keys(postObject);
-        console.log(keys, 'keys');
-
-        var ul = document.getElementById("pollings");
-
-        for (var i = 0; i < keys.length; i++) {
-            var currentObj = postObject[keys[i]];
-            var createdLi = crateElement(`${currentObj.heading}`, 'LI');
-            ul.appendChild(createdLi);
-        }
-
-            /* var dataArray = [];
-            var pollData = data.val();
-            for (var key in pollData) {
-                console.log(pollData[key])
-                dataArray.push(pollData);
+    firebaseDB.child("AppData/Polling/").once("value")
+        .then(function (result) {
+            var postObject = result.val();       /*we call all the data from appdata*/
+            var keys = Object.keys(postObject);  /*we are again asking for todo unique key so that we can run our loop till the end of todo list*/
+            console.log(keys);
+            for (let i = 0; i < keys.length; i++) {
+                let currentObject = postObject[keys[i]];  /*we got the first todo and enter to the first todo*/
+                console.log(currentObject);
+                let voteBTN = document.createElement("button");
+                let li = document.createElement("LI");
+                var buttontext = document.createTextNode("Vote");
+                voteBTN.appendChild(buttontext);
+                voteBTN.className = 'btn btn-info'
+                voteBTN.setAttribute('id','voteBTN')
+                let poleText = document.createTextNode(`${currentObject.heading}`)
+                li.appendChild(poleText)
+                li.appendChild(voteBTN)
+                ul.appendChild(li);
+                checkUID();
+                voteBTN.addEventListener('click', event => {
+                    let uid = firebase.auth().currentUser.uid;
+                    var obj = {uid: uid}
+                    firebaseDB.child("AppData/Voting/" + currentObject.heading + "/").push(obj);
+                    // this.style.display = 'none'
+                    console.log(keys[i]);
+                })
             }
+        })
+}
+
+function checkUID() {
+    firebaseDB.child("AppData/Voting/").once('value')
+        .then((data)=>{
+            var arrObj = [];
+            var objData = data.val();
+            console.log(objData);
+            for (var key in objData) {
+                console.log(objData[key])
+                for (var pol in objData[key]) {
+                    arrObj.push(objData[key][pol])
+                    console.log(objData[key][pol])
+                }
+            }
+            var userUID = firebase.auth().currentUser.uid;
+            console.log(arrObj)
+            arrObj.map((v,i)=>{
+                if (v.uid === userUID) {
+                    document.getElementById('voteBTN').style.display='none';
+                }
+                console.log(v.uid)
+            })
+        })
     
-            console.log(dataArray);
-            var ul = document.getElementById("pollings");
-            dataArray.map((v, i) => {
-                // if(){
-                console.log(v)
-                var createdLi = crateElement(`${v.heading}`, 'LI');
-                ul.appendChild(createdLi)
-                // }
-            }); */
-
-            // console.log(data, '[all data]')
-        });
-}
-function crateElement(text, element) {
-    var li = document.createElement(element);
-    var textNode = document.createTextNode(text);
-    li.appendChild(textNode);
-    var btn = document.createElement('BUTTON');
-    btn.className = 'btn btn-info'
-    var btnText = document.createTextNode('VOTE')
-    btn.appendChild(btnText)
-    li.appendChild(btn)
-    // li.setAttribute('class', className);
-    return li;
 }
 
+// getData();
+// function crateElement(text, element) {
+//     var li = document.createElement(element);
+//     var textNode = document.createTextNode(text);
+//     li.appendChild(textNode);
+//     var btn = document.createElement('BUTTON');
+//     btn.setAttribute('id', 'voteBTN');
+//     btn.className = 'btn btn-info'
+//     var btnText = document.createTextNode('VOTE')
+//     btn.appendChild(btnText)
+//     li.appendChild(btn)
+//     // li.setAttribute('class', className);
+//     return li;
+// }
 
-/* setting db */
 
